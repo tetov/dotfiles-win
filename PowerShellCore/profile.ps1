be475c3aa3a6cdfb535808509eb037cfa5758ed5
Anton Tetov <anton@tetov.xyz>
2021-11-10 12:00:30 +0100

    powershells

---
+++ PowerShellCore/profile.ps1
@@ -0,0 +1,9 @@
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\an6802jo\program\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+Import-Module 'C:\Users\an6802jo\program\scoop\apps\scoop\current\supporting\completion\Scoop-Completion.psd1' -ErrorAction SilentlyContinue
+
+$DotFilesPath = "C:\Users\an6802jo\dotfiles-win"
+$DotFilesAutodetect = $true
---
+++ WindowsPowerShell/profile.ps1
@@ -0,0 +1,4 @@
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\an6802jo\program\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
